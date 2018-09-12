cc1b176d52fa6024241a6b23920c172155905e24
autodocs <autodocs>
2018-09-12 01:16:58 +0000

    build based on 1fd1702

---
+++ latest/search_index.js
@@ -0,0 +1,43 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Mongoc.jl",
+    "title": "Mongoc.jl",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Mongoc.jl-1",
+    "page": "Mongoc.jl",
+    "title": "Mongoc.jl",
+    "category": "section",
+    "text": "MongoDB driver for the Julia Language.This is a thin wrapper around libmongoc, the official client library for C applications."
+},
+
+{
+    "location": "api.html#",
+    "page": "API",
+    "title": "API",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "api.html#Mongoc.BSONError",
+    "page": "API",
+    "title": "Mongoc.BSONError",
+    "category": "type",
+    "text": "Mirrors C struct bson_error_t. BSONError instances addresses are passed to libmongoc API using Ref{BSONError}(error), and are owned by the Julia process.\n\ntypedef struct {    uint32t domain;    uint32t code;    char message[504]; } bsonerrort;\n\n\n\n\n\n"
+},
+
+{
+    "location": "api.html#API-1",
+    "page": "API",
+    "title": "API",
+    "category": "section",
+    "text": "Modules = [Mongoc]"
+},
+
+]}
